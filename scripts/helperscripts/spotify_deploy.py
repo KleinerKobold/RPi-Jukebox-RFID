f811dd640f74201f7d075edbd06e7e11b0e1194a
oliver staudt <oliver.r.staudt@gmail.com>
2021-05-05 11:17:35 +0200

    tested and changed some minor stuff

--- scripts/helperscripts/spotify_deploy.py
+++ scripts/helperscripts/spotify_deploy.py
@@ -2,6 +2,6 @@ import os
 import yaml
 
 with open(r'spotify_streams.yaml') as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
+    config = yaml.load(file)
     for stream in config["streams"]:
-        os.system(f'./SpotifyCreate.sh -t {stream["title"]} -f {stream["folder"]} -c {stream["cardid"]} -s {stream["spotify"]}')
+        os.system(f'./CreateSpotify.sh --folder {stream["folder"].replace(" ","_")} --item {stream["title"].replace(" ","_")} --spotify {stream["spotify"]} --card {stream["cardid"]}')
